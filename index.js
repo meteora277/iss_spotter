412dca83d4c604a4cc7434238c23a953e36b7746
meteora277 <justin.s.diaz@gmail.com>
2021-12-02 14:35:11 -0500

    passed in second api callback to get geo location

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-const fetchMyIp = require("./fetchMyIp");
+const { fetchMyIp, fetchCoordsByIp } = require("./iss");
 
 fetchMyIp((err, data)=> {
-
   if (err) {
     console.log('your code is terrible!');
     return;
   }
-
   let ipAddress  = data;
-
   console.log(ipAddress);
 
-  
+  fetchCoordsByIp(ipAddress, (obj) =>{
+
+    console.log(obj);
+  });
 });
+
