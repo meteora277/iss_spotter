3fd3dd28ff999808eda2ac6b656da5e0bfd56467
meteora277 <justin.s.diaz@gmail.com>
2021-12-02 18:18:09 -0500

    created promise versions of fetchCoordinatesByIp and fetchFlyoverTimes

--- iss_promised.js
+++ iss_promised.js
@@ -1,1 +1,29 @@
 const request = require('request-promise-native');
+
+const fetchMyIp = function() {
+  
+
+  return request('https://api.ipify.org?format=json');
+
+};
+
+const fetchCoordsByIp = function(data) {
+
+  let ipAddress = JSON.parse(data).ip;
+  
+  return request(`https://api.freegeoip.app/json/${ipAddress}?apikey=109d8e50-53a5-11ec-8fbc-55cfc51e5d56`);
+
+};
+const fetchIssFlyoverTimes = function(locationData) {
+
+  let { longitude, latitude } = JSON.parse(locationData);
+  
+  return request(`https://iss-pass.herokuapp.com/json/?lat=${latitude}&lon=${longitude}`);
+
+};
+
+module.exports = {
+  fetchMyIp,
+  fetchCoordsByIp,
+  fetchIssFlyoverTimes
+};
