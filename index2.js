df75a4d87846f94be4653560e3c315a5802c2f64
meteora277 <justin.s.diaz@gmail.com>
2021-12-02 18:18:51 -0500

    added api calls to the promise chain

--- index2.js
+++ index2.js
@@ -1,1 +1,7 @@
-const request = require('request-promise-native');
+const { fetchMyIp, fetchCoordsByIp, fetchIssFlyoverTimes } = require("./iss_promised");
+
+
+fetchMyIp()
+  .then(fetchCoordsByIp)
+  .then(fetchIssFlyoverTimes)
+  .then((data) => console.log(JSON.parse(data).response));
