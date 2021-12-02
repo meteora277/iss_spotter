7aca5d268198f1ddb20851c455a7479a14d88bbd
meteora277 <justin.s.diaz@gmail.com>
2021-12-02 15:26:40 -0500

    add logic for fetching flyover times

--- iss.js
+++ iss.js
@@ -8,26 +8,18 @@ const fetchMyIp = function(callback) {
       callback(error, null);
     }
     if (status.statusCode !== 200) {
-
       let message = `Status Code ${status.statusCode} when fetching IP. Response: ${body}`;
-
       callback(Error(message), null);
-
     } else {
-
       let ipAddress = JSON.parse(body).ip;
-
       callback(null, ipAddress);
-
     }
-    
   });
 };
 
 const fetchCoordsByIp = function(ipAddress, callback) {
 
   request.get(`https://api.freegeoip.app/json/${ipAddress}?apikey=109d8e50-53a5-11ec-8fbc-55cfc51e5d56`, (err, status, body) => {
-
     if (err) {
       console.log(err);
     }
@@ -35,7 +27,6 @@ const fetchCoordsByIp = function(ipAddress, callback) {
       let message = `Status Code: ${status.statusCode}, ${body}`;
       callback(Error(message), null);
     } else {
-
       let data = JSON.parse(body);
       let longitude = data.longitude;
       let latitude = data.latitude;
@@ -46,7 +37,25 @@ const fetchCoordsByIp = function(ipAddress, callback) {
   
 };
 
+const fetchIssFlyoverTimes = function(geoCoordinates, callback) {
+  let { latitude, longitude } = geoCoordinates;
+  request.get(`https://iss-pass.herokuapp.com/json/?lat=${latitude}&lon=${longitude}`, (error, status, body) => {
+
+    if (error) {
+      console.log(error);
+    }
+    if (status.statusCode !== 200) {
+      let message = `Status Code: ${status.statusCode}, ${body}`;
+      callback(Error(message), null);
+    } else {
+      let flyoverTimes = JSON.parse(body).response;
+      callback(null, flyoverTimes);
+    }
+  });
+};
+
 module.exports = {
   fetchMyIp,
-  fetchCoordsByIp
+  fetchCoordsByIp,
+  fetchIssFlyoverTimes
 };
